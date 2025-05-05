6348baa1dcfce491ba98275d749787ade2239d7b
Abhishek Jindal <abhishek.jindal@saasworx.ai>
2025-05-05 16:49:33 +0530

    Implement OAuth 2

---
+++ src/main/java/org/example/functions/Authorization/OAuth2.java
@@ -0,0 +1,39 @@
+package org.example.functions.Authorization;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeServlet;
+import com.google.api.client.http.GenericUrl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OAuth2 extends AbstractAppEngineAuthorizationCodeServlet {
+
+    @Override
+    protected AuthorizationCodeFlow initializeFlow() throws IOException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest.BodyPublisher requestBodyPublisher = HttpRequest.BodyPublishers.ofString("grant_type=authorization_code");
+        HttpRequest request = HttpRequest.newBuilder(URI.create("https://accounts.google.com/o/oauth2/v2/auth")).POST(requestBodyPublisher).build();
+
+        try {
+            HttpResponse<InputStream> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    protected String getRedirectUri(HttpServletRequest request) throws ServletException, IOException {
+        GenericUrl redirectUrl = new GenericUrl(request.getRequestURL().toString());
+        redirectUrl.setRawPath("/oauth2Callback");
+        return redirectUrl.build();
+    }
+
+}
